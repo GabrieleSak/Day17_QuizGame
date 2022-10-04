d1443c43a6b928a34cb6fbeaf7b79a1cf2e51e6c
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-10-04 18:24:31 +0300

    Add new data from https://opentdb.com/ and modify Question initialisation to get questions from it

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    new_q = Question(question["text"], question["answer"])
+    new_q = Question(question["question"], question["correct_answer"])
     question_bank.append(new_q)
 
 quiz = QuizBrain(question_bank)
